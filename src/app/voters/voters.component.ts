0e2ffbb4f6a081f29322f5c03b62d2089812859b
pmroczko <58851399+pmroczko@users.noreply.github.com>
2023-11-26 14:58:14 +0100

    Created simple voter table with mocks.

--- src/app/voters/voters.component.ts
+++ src/app/voters/voters.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+
+export interface Voter {
+  name: string;
+  hasVoted: boolean;
+}
 
 @Component({
   selector: 'app-voters',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './voters.component.html',
   styleUrl: './voters.component.scss'
 })
-export class VotersComponent {
 
+export class VotersComponent {
+  displayedColumns = ['name', 'hasVoted'];
+  voters: Voter[] = [ 
+    {name: 'voter1', hasVoted: false}, 
+    {name: 'voter2', hasVoted: false}, 
+    {name: 'voter3', hasVoted: false}
+  ]
 }
